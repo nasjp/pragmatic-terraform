c207c683b0905c6db866ddde6976fe908912f243
nas <nas.silver.bullet@gmail.com>
2021-03-08 21:56:43 +0900

    fix

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+/* iam */
+module "describe_regins_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+/* security group */
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
