60c8df4b1e693d84be9d3428707de5e223a6c954
nas <nas.silver.bullet@gmail.com>
2021-03-08 15:24:18 +0900

    fix

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,8 @@
-locals {
-  example_instance_type      = "t3.micro"
-  example_instance_type_prod = "m5.large"
+provider "aws" {
+  region = "ap-northeast-1"
 }
 
-module "dev_server" {
-  source        = "./http_server"
-  instance_type = local.example_instance_type
-}
-
-output "public_dns" {
-  value = module.dev_server.public_dns
+resource "aws_instance" "example" {
+  ami           = "ami-0f9ae750e8274075b"
+  instance_type = "t3.micro"
 }
